4c78bdaef25e12978866dbf0393a27b5c359d12d
Arman Alimov <527c@mail.ru>
2022-02-17 23:48:42 +0600

    new task

--- jsTask.js
+++ jsTask.js
@@ -1,30 +1,55 @@
 ///////////////////////////////////////////////////////////////
 
-let pr1 = new Promise((res) => {
-    res(10)
-})
-let pr2 = new Promise((res) => {
-    res(0)
+// let pr1 = new Promise((res) => {
+//     res(10)
+// })
+// let pr2 = new Promise((res) => {
+//     res(0)
+// })
+//
+// pr1
+//     .then((res) => {
+//         console.log(res)
+//         return res + 2
+//     })
+//     .then((res) => {
+//         console.log(res)
+//         return res + 2
+//     })
+//     .then(console.log)
+//
+// pr2
+//     .then((res) => {
+//         console.log(res)
+//         return res + 1
+//     })
+//     .then((res) => {
+//         console.log(res)
+//         return res + 1
+//     })
+//     .then(console.log)
+
+///////////////////////////////////////////////////////////////
+
+(
+    function (){
+        setTimeout(() => console.log(1), 100)
+    }
+)();
+
+console.log(2)
+
+let i = 0
+
+while (i < 50000000) {
+    i++
+}
+
+new Promise((res, rej) => {
+    setTimeout(() => console.log(3), 50);
 })
 
-pr1
-    .then((res) => {
-        console.log(res)
-        return res + 2
-    })
-    .then((res) => {
-        console.log(res)
-        return res + 2
-    })
-    .then(console.log)
-
-pr2
-    .then((res) => {
-        console.log(res)
-        return res + 1
-    })
-    .then((res) => {
-        console.log(res)
-        return res + 1
-    })
-    .then(console.log)
+function f() {
+    console.log(4)
+}
+Promise.resolve(console.log(5))
